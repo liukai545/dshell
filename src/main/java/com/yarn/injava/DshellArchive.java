b96caca6d7023684f8a765ab8b3b40da2df7bc7d
liukai <liukai0293@126.com>
2016-01-08 19:02:50 +0800

    add scala impl

---
+++ src/main/java/com/yarn/injava/DshellArchive.java
@@ -0,0 +1,33 @@
+package com.yarn.injava;
+
+public class DshellArchive {
+
+	public String archivePath = "";
+	public Long timestamp;
+	public Long size;
+
+	public String getArchivePath() {
+		return archivePath;
+	}
+
+	public void setArchivePath(String archivePath) {
+		this.archivePath = archivePath;
+	}
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public Long getSize() {
+		return size;
+	}
+
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
+}
---
+++ src/main/java/com/yarn/inscala/HelloWorld.scala
@@ -0,0 +1,11 @@
+package com.yarn.inscala
+
+/**
+  * Created by liukai on 2016/1/8.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    println("hello world")
+  }
+}
